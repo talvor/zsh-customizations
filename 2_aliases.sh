a00b6e99fca64ecd288d7ca9cffdb9b86e9b7cb0
Phillip Hall <phall@atlassian.com>
2020-12-09 09:00:00 +1100

    Move personal aliases to private file

--- 2_aliases.sh
+++ 2_aliases.sh
@@ -1,23 +1,16 @@
-alias edit-develop="$PROJECT_EDITOR ~/.zsh-customizations/2_aliases.sh"
+alias edit-aliases="$PROJECT_EDITOR ~/.zsh-customizations/2_aliases.sh"
 
 # CD Aliases
 alias cd-atlassian="cd ~/Development/Atlassian"
-alias cd-personal="cd ~/Development/Personal"
 alias cd-projects="cd ~/Development/Atlassian/Projects"
 alias cd-playground="cd ~/Development/Atlassian/Playground"
 alias cd-jira-frontend="cd ~/Development/Atlassian/Projects/jira-frontend"
 alias cd-xflow-ui="cd ~/Development/Atlassian/Projects/xflow-ui"
 alias cd-online-courses="cd ~/Development/Atlassian/Online\ Courses"
 alias cd-workspace="cd ~/Workspace"
-alias cd-gitlab="cd ~/Development/Personal/Gitlab"
 
 # Aliases to open folders/files in vscode
 alias edit-zsh-customizations="$PROJECT_EDITOR ~/.zsh-customizations"
-alias edit-notes="$PROJECT_EDITOR ~/Development/Atlassian/Notes"
-
-# Display settings aliases
-alias display-workdesk="autorandr --load workdesk"
-alias display-undocked="autorandr --load undocked"
 
 # Alias to reload this file
 alias reload-aliases="echo \"Reloading alias customizations\" && source $0"
