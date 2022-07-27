a8a0d182e64b547ccc26b7bd887bd39b359b7293
Phillip Hall <phall@atlassian.com>
2022-07-27 15:39:42 +1000

    Add load-py-env alias

--- 2_aliases.sh
+++ 2_aliases.sh
@@ -4,10 +4,7 @@ alias edit-aliases="$PROJECT_EDITOR ~/.zsh-customizations/2_aliases.sh &"
 alias cd-atlassian="cd ~/Development/Atlassian"
 alias cd-projects="cd ~/Development/Atlassian/Projects"
 alias cd-playground="cd ~/Development/Atlassian/Playground"
-alias cd-jira-frontend="cd ~/Development/Atlassian/Projects/jira-frontend"
-alias cd-xflow-ui="cd ~/Development/Atlassian/Projects/xflow-ui"
 alias cd-online-courses="cd ~/Development/Atlassian/Online\ Courses"
-alias cd-workspace="cd ~/Workspace"
 
 # Aliases to open folders/files in vscode
 alias edit-zsh-customizations="$PROJECT_EDITOR ~/.zsh-customizations &"
@@ -21,3 +18,5 @@ alias plantuml='f() { java -jar ~/.java/plantuml.jar  $1 };f'
 
 alias docker-kill-all="docker ps | cut -d ' ' -f 1 | xargs docker kill"
 alias docker-rm-all="docker ps -a | cut -d ' ' -f 1 | xargs docker rm"
+
+alias load-py-env="source ${HOME}/Development/Atlassian/Python/bin/activate"
